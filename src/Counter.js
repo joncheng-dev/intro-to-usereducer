970707d6ce5d0bd85743e0291fb0ee4514cc7e5b
Jonathan Cheng <jcheng461@gmail.com>
2023-12-10 09:49:07 -0800

    add actions to decrement counter and reset counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,59 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
+
+const initialState = {
+  counter: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+      };
+    case "reset":
+      return {
+        counter: (state.counter = 0),
+      };
+    default:
+      throw new Error(`There is no matching action: ${action.type}`);
+  }
+}
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [hidden, setHidden] = useState(false);
 
   useEffect(() => {
-    document.title = counter;
-  }, [counter]);
+    document.title = state.counter;
+  }, [state.counter]);
 
   return (
     <React.Fragment>
-      {hidden ? <h1>Count Hidden</h1> : <h1>{counter}</h1>}
       <button
         onClick={() => {
-          setCounter(counter + 1);
+          dispatch({ type: "decrement" });
+        }}
+      >
+        Decrement
+      </button>
+      {hidden ? <h1>Count Hidden</h1> : <h1>{state.counter}</h1>}
+      <button
+        onClick={() => {
+          dispatch({ type: "increment" });
         }}
       >
         Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "reset" });
+        }}
+      >
+        Reset Counter
       </button>
       <button onClick={() => setHidden(!hidden)}>Hide/Show</button>
     </React.Fragment>
